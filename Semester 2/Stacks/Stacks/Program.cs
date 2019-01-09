81d14a093e2a65b1545c910d8ae5d34978774583
ThatOneUsername343 <legokidram@gmail.com>
2019-01-09 14:37:35 -0700

    no message

--- Semester 2/Stacks/Stacks/Program.cs
+++ Semester 2/Stacks/Stacks/Program.cs
@@ -10,11 +10,38 @@ namespace Stacks
     {
         static void Main(string[] args)
         {
+            int myInt;
+            int Input;
             Stack myStack = new Stack();
 
-            myStack.Push(8);
-            myStack.Push(6);
-            
+            do
+            {
+                Console.WriteLine();
+                Console.WriteLine("1. Add something to the top of the stack");
+                Console.WriteLine("2. Remove the top element of the stack");
+                Console.WriteLine("3. return the top element of the stack");
+                Console.WriteLine("4. Exit this thing.");
+                Input = int.Parse(Console.ReadLine());
+
+                if (Input == 1)
+                {
+                    Console.WriteLine("Type the value you would like to add.");
+                    myInt = int.Parse(Console.ReadLine());
+                    myStack.Push(myInt);
+                }
+
+                if (Input == 2)
+                {
+                    myInt = myStack.Pop();
+                    Console.WriteLine(myInt + " is the new top value.");
+                }
+
+                if (Input == 3)
+                {
+                    myInt = myStack.Peek();
+                    Console.WriteLine(myInt + " is the new top value.");
+                }
+            } while (Input != 4);
         }
     }
 }
--- Semester 2/Stacks/Stacks/Stack.cs
+++ Semester 2/Stacks/Stacks/Stack.cs
@@ -18,25 +18,42 @@ namespace Stacks
         //Push
         public void Push(int val)
         {
-
+            holder.Add(val);
         }
 
         //Pop
         public int Pop()
         {
-            return 0;
+            if (holder.Count > 0)
+            {
+                int myInt;
+                myInt = holder.Last();
+                holder.RemoveAt(holder.Count() - 1);
+                return myInt;
+            }
+
+            else
+            {
+                Console.WriteLine("STACK UNDERFLOW BIACH!!!!");
+                return 0;
+            }
         }
 
         //Peek
         public int Peek()
         {
-            return 0;
+            int myInt;
+            myInt = holder.Last();
+            return myInt;
         }
 
         //Print
         public void Print()
         {
-
+            for (int i = 0; i < holder.Count; i++)
+            {
+                Console.WriteLine(holder[i] + ",");
+            }
         }
     }
 }
