5d9ec9fffde872cff1cc6f2e2b0bd215225792b1
ThatOneUsername343 <44710761+ThatOneUsername343@users.noreply.github.com>
2019-02-18 13:40:17 -0700

    Made changes to Algorithm Practice 4

--- Semester 2/Algorithm Practice/Algorithm Practice 4/Algorithm Practice 4/Program.cs
+++ Semester 2/Algorithm Practice/Algorithm Practice 4/Algorithm Practice 4/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Algorithm_Practice_4
 {
@@ -10,7 +11,32 @@ namespace Algorithm_Practice_4
     {
         static void Main(string[] args)
         {
+            //int i, fact = 1, number;
+            //Console.Write("Enter any Number: ");
+            //number = int.Parse(Console.ReadLine());
+            //for (i = 1; i <= number; i++)
+            //{
+            //    fact = fact * i;
+            //}
 
+            string path = AppDomain.CurrentDomain.BaseDirectory + @"Prob04.in_.txt";
+
+            using (StreamReader Strem = new StreamReader(path))
+            {
+                string line = Strem.ReadLine();
+                int i;
+                float ThatOneFloat = 3, number = float.Parse(line);
+                ThatOneFloat = number;
+                while ((line = Strem.ReadLine()) != null)
+                {
+                    for (i = line; i <= number; i++)
+                    {
+                        ThatOneFloat = ThatOneFloat * number;
+                    }
+                }
+                Console.Write("Factorial of " + number + " is: " + ThatOneFloat);
+                Console.ReadLine();
+            }
         }
     }
 }
